7ecae46a5dc1f61b0dbe36be43da15135ed2e97a
f.taka <98.tenpwayway@gmail.com>
2020-08-12 00:52:00 +0900

    Implement advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_125610) do
+ActiveRecord::Schema.define(version: 2020_08_11_144508) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_08_11_125610) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
