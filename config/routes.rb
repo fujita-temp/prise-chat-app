f6d1e949fdc78a0e59118f3f5f5d5f84edfc79e1
f.taka <98.tenpwayway@gmail.com>
2020-08-11 23:42:47 +0900

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
+  resources:users
 
   #rootが変更された時のための保険
   #get '/signup', to:'users#new'
 
-  resources:users
 end
--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,19 +1,7 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-
-  def setup
-    @user = users(:michael)
-    # このコードは慣習的に正しくない
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
-  end
-
-  test "should be valid" do
-    assert @micropost.valid?
-  end
-
-  test "user id should be present" do
-    @micropost.user_id = nil
-    assert_not @micropost.valid?
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
